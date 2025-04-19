d4a43aac46786014e5719be895223af8780b5256
OwenDelBene <owen.delbene@gmail.com>
2025-04-19 09:23:24 -0500

    drive train speeds

--- PCA9685/driveConfig.h
+++ PCA9685/driveConfig.h
@@ -8,33 +8,38 @@
 #define DC_R1 6
 #define DC_R2 7
 
+//Forward (0,2750)
+//Backward (2750, 5500)
 
-#define L1_F 100
-#define L1_B 100
-#define L1_L 100
-#define L1_R 100
+#define D 1375
+#define R 4125
+
+#define L1_F D
+#define L1_B R
+#define L1_L R
+#define L1_R D
 
 
-#define L2_F 100
-#define L2_B 100
-#define L2_L 100
-#define L2_R 100 
+#define L2_F D
+#define L2_B R
+#define L2_L R
+#define L2_R D 
 
-#define R1_F 100
-#define R1_B 100
-#define R1_L 100
-#define R1_R 100
+#define R1_F D
+#define R1_B R
+#define R1_L D
+#define R1_R R
 
-#define R2_F 100
-#define R2_B 100
-#define R2_L 100
-#define R2_R 100
+#define R2_F D
+#define R2_B R
+#define R2_L D
+#define R2_R R
 
 
 #define ms_F 1000
 #define ms_B 1000
-#define ms_L 1000
-#define ms_R 1000
+#define ms_L  250
+#define ms_R  250
 
 
 #define DRIVE_TRAIN_CAP 5
